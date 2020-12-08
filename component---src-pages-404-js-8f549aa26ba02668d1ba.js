e9724b2d79f7dbcdf9cc34b133928b688c200167
Diego Dorado <diegodorado@gmail.com>
2020-12-08 15:30:54 -0300

    Updates

---
+++ component---src-pages-404-js-8f549aa26ba02668d1ba.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),c=t("Bl7J"),r=t("vrFN");n.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"404"}),l.a.createElement("section",null,l.a.createElement("h2",null,"404"),l.a.createElement("p",null,"Página no encontrada.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-8f549aa26ba02668d1ba.js.map
